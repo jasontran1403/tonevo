8f5843e638791e370d325edffd44070e7d7ebf15
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-10-03 16:30:56 +0700

    update layout coming

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -3,17 +3,17 @@ import "../404assets/coming.css";
 const CommingSoon = () => {
   return (
     <div id="notfound">
-      <div className="notfound">
+    <div className="notfound">
         <div className="notfound-404">
-          <h1>Coming Soon!</h1>
+            <h1>Coming Soon!</h1>
         </div>
         {/* Move <p> and <a> to the bottom */}
-        <div className="remove"><p>
-          We're working hard to bring you this page. Stay tuned for updates.
-        </p>
-        <a href="/">Go to Landing page</a></div>
-      </div>
+        <div className="remove">
+            <a href="/">Go to Landing page</a>
+        </div>
     </div>
+</div>
+
   );
 };
 
