72ea16ce698ceb7d585aa29b742ba1bd0457a94d
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-10-10 09:02:29 +0700

    update system

--- src/routes.jsx
+++ src/routes.jsx
@@ -58,137 +58,141 @@ export default function Router() {
             element: <LandingPage />
         },
         {
-            path: "/clip",
-            element: <LandingPage2 />
-        },
-        {
-            path: "/dashboard",
-            element: isConnectedToWallet ? <Dashboard /> : <Navigate to="/" />
-        },
-        {
-            path: "/staking",
-            element: isConnectedToWallet ? <Investment /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-usdt",
-            element: isConnectedToWallet ? <WithdrawUSDT /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-mct",
-            element: isConnectedToWallet ? <WithdrawMCT /> : <Navigate to="/" />
-        },
-        {
-            path: "/deposit-usdt",
-            element: isConnectedToWallet ? <DepositUSDT /> : <Navigate to="/" />
-        },
-        {
-            path: "/deposit-mct",
-            element: isConnectedToWallet ? <DepositMCT /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-usdt-mct",
-            element: isConnectedToWallet ? <SwapUsdtMCT /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-mct-usdt",
-            element: isConnectedToWallet ? <SwapMCTUsdt /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-daily",
-            element: isConnectedToWallet ? <SwapDaily /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-direct",
-            element: isConnectedToWallet ? <SwapDirect /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-binary",
-            element: isConnectedToWallet ? <SwapBinary /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-leader",
-            element: isConnectedToWallet ? <SwapLeader /> : <Navigate to="/" />
-        },
-        {
-            path: "/swap-pop",
-            element: isConnectedToWallet ? <SwapPop /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-pop",
-            element: isConnectedToWallet ? <WithdrawPop /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-daily",
-            element: isConnectedToWallet ? <WithdrawDaily /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-direct",
-            element: isConnectedToWallet ? <WithdrawDirect /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-binary",
-            element: isConnectedToWallet ? <WithdrawBinary /> : <Navigate to="/" />
-        },
-        {
-            path: "/withdraw-leader",
-            element: isConnectedToWallet ? <WithdrawLeader /> : <Navigate to="/" />
-        },
-        {
-            path: "/transfer-direct",
-            element: isConnectedToWallet ? <TransferDirect /> : <Navigate to="/" />
-        },
-        {
-            path: "/transfer-binary",
-            element: isConnectedToWallet ? <TransferBinary /> : <Navigate to="/" />
-        },
-        {
-            path: "/transfer-leader",
-            element: isConnectedToWallet ? <TransferLeader /> : <Navigate to="/" />
-        },
-        {
-            path: "/transfer-pop",
-            element: isConnectedToWallet ? <TransferPop /> : <Navigate to="/" />
-        },
-        {
-            path: "/transfer-daily",
-            element: isConnectedToWallet ? <TransferDaily /> : <Navigate to="/" />
-        },
-        {
-            path: "/direct-tree",
-            element: isConnectedToWallet ? <DirectTree /> : <Navigate to="/" />
-        },
-        {
-            path: "/test",
-            element: <Test />
-        },
-        {
-            path: "/transfer",
-            element: isConnectedToWallet ? <Transfer /> : <Navigate to="/" />
-        },
-        {
-            path: "/transactions",
-            element: isConnectedToWallet ? <Transactions /> : <Navigate to="/" />
-        },
-        {
-            path: "/tree",
-            element: isConnectedToWallet ? <Tree /> : <Navigate to="/" />
-        },
-        {
-            path: "/disconnect",
-            element: <DisconnectComponent />
-        },
-        {
-            path: "/mapchain-swap",
+            path: "*",
             element: <CommingSoon />
-        },
-        {
-            path: '/404',
-            element: <Error404 />
-        },
-        {
-            path: '*',
-            element: <Navigate to="/404" replace />
-        },
+        }
+        // {
+        //     path: "/clip",
+        //     element: <LandingPage2 />
+        // },
+        // {
+        //     path: "/dashboard",
+        //     element: isConnectedToWallet ? <Dashboard /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/staking",
+        //     element: isConnectedToWallet ? <Investment /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-usdt",
+        //     element: isConnectedToWallet ? <WithdrawUSDT /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-mct",
+        //     element: isConnectedToWallet ? <WithdrawMCT /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/deposit-usdt",
+        //     element: isConnectedToWallet ? <DepositUSDT /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/deposit-mct",
+        //     element: isConnectedToWallet ? <DepositMCT /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-usdt-mct",
+        //     element: isConnectedToWallet ? <SwapUsdtMCT /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-mct-usdt",
+        //     element: isConnectedToWallet ? <SwapMCTUsdt /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-daily",
+        //     element: isConnectedToWallet ? <SwapDaily /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-direct",
+        //     element: isConnectedToWallet ? <SwapDirect /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-binary",
+        //     element: isConnectedToWallet ? <SwapBinary /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-leader",
+        //     element: isConnectedToWallet ? <SwapLeader /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/swap-pop",
+        //     element: isConnectedToWallet ? <SwapPop /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-pop",
+        //     element: isConnectedToWallet ? <WithdrawPop /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-daily",
+        //     element: isConnectedToWallet ? <WithdrawDaily /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-direct",
+        //     element: isConnectedToWallet ? <WithdrawDirect /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-binary",
+        //     element: isConnectedToWallet ? <WithdrawBinary /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/withdraw-leader",
+        //     element: isConnectedToWallet ? <WithdrawLeader /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transfer-direct",
+        //     element: isConnectedToWallet ? <TransferDirect /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transfer-binary",
+        //     element: isConnectedToWallet ? <TransferBinary /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transfer-leader",
+        //     element: isConnectedToWallet ? <TransferLeader /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transfer-pop",
+        //     element: isConnectedToWallet ? <TransferPop /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transfer-daily",
+        //     element: isConnectedToWallet ? <TransferDaily /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/direct-tree",
+        //     element: isConnectedToWallet ? <DirectTree /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/test",
+        //     element: <Test />
+        // },
+        // {
+        //     path: "/transfer",
+        //     element: isConnectedToWallet ? <Transfer /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/transactions",
+        //     element: isConnectedToWallet ? <Transactions /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/tree",
+        //     element: isConnectedToWallet ? <Tree /> : <Navigate to="/" />
+        // },
+        // {
+        //     path: "/disconnect",
+        //     element: <DisconnectComponent />
+        // },
+        // {
+        //     path: "/mapchain-swap",
+        //     element: <CommingSoon />
+        // },
+        // {
+        //     path: '/404',
+        //     element: <Error404 />
+        // },
+        // {
+        //     path: '*',
+        //     element: <Navigate to="/404" replace />
+        // },
     ]);
 
     return routes;
