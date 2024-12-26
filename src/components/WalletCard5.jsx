2448c1acc34fda08372c02e662fa991a6f548c1d
jasontran1403 <hainguyentran14@gmail.com>
2024-12-26 20:19:02 +0700

    update 2

--- src/components/WalletCard5.jsx
+++ src/components/WalletCard5.jsx
@@ -20,14 +20,14 @@ const WalletCard5 = ({ content, amount, unit, rank, wallet }) => {
         <span className="text-sm text-gray-500 dark:text-gray-400">
           {formatNumber(amount)} {unit}
         </span>
-        <div className="flex mt-4 md:mt-6">
-            <a
-              href={isAdmin ? "/admin/transfer/"+id : "/transfer"}
-              className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            >
-              Transfer
-            </a>
-          </div>
+        {/* <div className="flex mt-4 md:mt-6">
+          <a
+            href={isAdmin ? "/admin/transfer/" + id : "/transfer"}
+            className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Transfer
+          </a>
+        </div> */}
       </div>
     </div>
   );
